f545e945f5fc88abee8add44c24fcb369863be42
小鱼周凌宇 <coderfish@163.com>
2017-08-17 17:07:26 +0800

    更新 Demo，添加源文件

--- Demo/FrameKitDemo/ViewController/ViewController.swift
+++ Demo/FrameKitDemo/ViewController/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let _ = self.view.fkit.x
+        self.view.fkit.x = 0.0
+
+        let _ = self.view.fkit.right
+        self.view.fkit.right = 300.0
+        
+        let _ = self.view.fkit.bottom
+        self.view.fkit.bottom = 300.0
+
+        let _ = self.view.fkit.centerX
+        self.view.fkit.centerX = 100.0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
